9b81da31484957992a9b3f66f99e4233bc9e9e57
Scott Talbot <s@chikachow.org>
2016-03-28 19:05:03 +1100

    hack

--- Sources/Build/Command.compile(SwiftModule).swift
+++ Sources/Build/Command.compile(SwiftModule).swift
@@ -15,7 +15,18 @@ import PkgConfig
 extension Command {
     static func compile(swiftModule module: SwiftModule, configuration conf: Configuration, prefix: String, otherArgs: [String], SWIFT_EXEC: String) throws -> Command {
 
-        let otherArgs = otherArgs + module.XccFlags(prefix) + (try module.pkgConfigSwiftcArgs()) + module.moduleCacheArgs(prefix: prefix)
+        let otherArgs = otherArgs + module.XccFlags(prefix) + (try module.pkgConfigSwiftcArgs()) + module.moduleCacheArgs(prefix: prefix) + recursiveDependencies([module]).flatMap({ (module: Module) -> [String] in
+            switch module {
+            case is SwiftModule:
+                return []
+            case is ClangModule:
+                return ["-l\(module.c99name)"]
+            case let module as CModule:
+                return []
+            case _:
+                return []
+            }
+        })
         var args = ["-j\(SwiftcTool.numThreads)", "-D", "SWIFT_PACKAGE"]
 
         switch conf {
